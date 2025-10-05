f910edf42589f4bf46f019d405ea3e7a11fee5bc
muhammadanas <anas1255.ma@gmail.com>
2025-10-05 21:08:34 +0500

    Feat: Sign up screen validators added

--- lib/features/onboarding/presentation/views/signup_screen.dart
+++ lib/features/onboarding/presentation/views/signup_screen.dart
@@ -14,7 +14,8 @@ import 'package:plugdin/utils/widgets/core_widgets/export.dart';
 class SignupScreen extends StatelessWidget {
   SignupScreen({super.key});
 
-  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _fullNameController = TextEditingController();
+  final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPasswordController =
@@ -37,77 +38,90 @@ class SignupScreen extends StatelessWidget {
             horizontal: 16,
             vertical: 24,
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Enter Your Details',
-                style: context.h1,
-              ),
-              const SizedBox(
-                height: 4,
-              ),
-              Text(
-                'This is what will be shown on your profile. Make sure to use your authentic details to help us create a trustworthy community.',
-                style: context.b2.copyWith(
-                  color: AppColors.darkGreyTextColor,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Enter Your Details',
+                  style: context.h1,
                 ),
-              ),
-              const SizedBox(
-                height: 32,
-              ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  'This is what will be shown on your profile. Make sure to use your authentic details to help us create a trustworthy community.',
+                  style: context.b2.copyWith(
+                    color: AppColors.darkGreyTextColor,
+                  ),
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
 
-              PITextField(
-                hintText: 'Full Name',
-                controller: _nameController,
-                validator: FieldValidators.nameValidator,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
+                PITextField(
+                  hintText: 'Full Name',
+                  controller: _fullNameController,
+                  validator: FieldValidators.nameValidator,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
 
-              PITextField(
-                hintText: 'Email',
-                controller: _emailController,
-                validator: FieldValidators.emailValidator,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
+                // PITextField(
+                //   hintText: 'User Name',
+                //   controller: _userNameController,
+                //   validator: FieldValidators.nameValidator,
+                // ),
+                // const SizedBox(
+                //   height: 16,
+                // ),
+                PITextField(
+                  hintText: 'Email',
+                  controller: _emailController,
+                  validator: FieldValidators.emailValidator,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
 
-              PITextField(
-                hintText: 'Password',
-                controller: _passwordController,
-                validator: FieldValidators.passwordValidator,
-                type: PITextFieldType.password,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              PITextField(
-                hintText: 'Confirm Password',
-                controller: _confirmPasswordController,
-                validator: FieldValidators.passwordValidator,
-                type: PITextFieldType.password,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-            ],
+                PITextField(
+                  hintText: 'Password',
+                  controller: _passwordController,
+                  validator: FieldValidators.passwordValidator,
+                  type: PITextFieldType.password,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                PITextField(
+                  hintText: 'Confirm Password',
+                  controller: _confirmPasswordController,
+                  validator: FieldValidators.passwordValidator,
+                  type: PITextFieldType.password,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+              ],
+            ),
           ),
         ),
         bottomNavigationBar: BlocBuilder<OnboardingCubit, OnboardingState>(
           builder: (context, state) {
             return SafeArea(
               child: PIButton(
-                text: 'Next',
+                text: 'Sign Up',
                 onPressed: () {
-                  context.read<OnboardingCubit>().emailSignUp(
-                    fullName: _nameController.text.trim(),
-                    email: _emailController.text.trim(),
-                    password: _passwordController.text,
-                    role: state.selectedRoleType ?? RoleType.customer,
-                  );
+                  if (_formKey.currentState!.validate()) {
+                    context.read<OnboardingCubit>().emailSignUp(
+                      fullName: _fullNameController.text.trim(),
+                      email: _emailController.text.trim(),
+                      password: _passwordController.text,
+                      role: state.selectedRoleType ?? RoleType.customer,
+                    );
+                  }
                 },
                 isLoading: state.emailSignUp.isLoading,
                 outsidePadding: const EdgeInsetsDirectional.symmetric(
